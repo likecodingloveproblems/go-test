a6d890d0591844ab20f950c060a26a92e1837dc3
Mohammad Ali Mehdizadeh <mehdizadeh.miare@gmail.com>
2023-10-25 16:18:50 +0330

    feat: two articles added

--- main.go
+++ main.go
@@ -31,6 +31,10 @@ type Trip struct {
 }
 
 func (t *Trip) End() error {
+	// I want to handle this by types
+	// the idea comes from this two papers
+	// https://blog.janestreet.com/effective-ml-revisited/
+	// https://stianlagstad.no/2022/05/parse-dont-validate-python-edition/#:~:text=The%20point%20in%20%E2%80%9CParse%2C%20don,sure%20everything%20is%20in%20order.
 	if t.State != AssignedTrip || t.AssignedAt.IsZero() || t.Assignee == "" {
 		return errors.New("only assigned trip can be ended!")
 	}
